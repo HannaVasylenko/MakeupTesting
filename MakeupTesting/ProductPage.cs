d1fd1ead90123a755915ccfae9e24f530b99b7e9
HannaVasylenko <vasilenko12345@gmail.com>
2024-03-13 16:17:58 +0200

    1. Created ProductTests 2. Initialized web elements and created methods on ProductPage 3. Add a test VerifyPossibilityToSelectProductVariants

--- MakeupTesting/ProductPage.cs
+++ MakeupTesting/ProductPage.cs
@@ -16,11 +16,36 @@ namespace MakeupTestingPageObjects
         }
 
         private IWebElement btnBuy => webDriver.FindElement(By.XPath("//div[@class='button buy']"));
+        private IWebElement titleProductVariant(string productVariant) => webDriver.FindElement(By.XPath($"//label[@class='product-variant-selected']//span[contains(text(),'{productVariant}')]"));
+
+        private IWebElement ddProductVariants => webDriver.FindElement(By.XPath("//div[@class='select']"));
+
+        private List<IWebElement> productVariantsList => webDriver.FindElements(By.XPath("//div[@class='variants scrolling full-width']/div")).ToList();
+        public string GetProductVariantText(string productVariant) => titleProductVariant(productVariant).Text;
+
+
         public void AddProductToCart()
         {
             WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
             wait.Until(x => btnBuy.Displayed);
             btnBuy.Click();
+        }
+       
+        public void SelectProductVariants(string variantText)
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+            wait.Until(x => ddProductVariants.Displayed);
+            ddProductVariants.Click();
+
+            foreach (var product in productVariantsList)
+            {
+                string productTitle = product.FindElement(By.XPath("./span")).Text;
+                if (productTitle.Contains(variantText))
+                {
+                    product.Click();
+                    break;
+                }
+            }
         }
     }
 }
---
+++ MakeupTestingTests/ProductTests.cs
@@ -0,0 +1,33 @@
+﻿using MakeupTestingPageObjects;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeupTestingTests
+{
+    public class ProductTests : BaseTest
+    {
+        [Test]
+        public void VerifyPossibilityToSelectProductVariants()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appconfig.json").Build();
+            string titleproductVariant = config["productVariant"];
+
+            Header header = new Header(driver);
+            header.SelectCategory(config["category"]);
+            DecorativeСosmeticsPage decorativeCosmetics = new DecorativeСosmeticsPage(driver);
+            decorativeCosmetics.CheckFiltersByNameAndTypeOfProduct(config["nameOfBrand"], config["filterProductName"]);
+            decorativeCosmetics.SelectProductCard(config["productAddToCart2"]);
+            ProductPage productPage = new ProductPage(driver);
+            productPage.SelectProductVariants(config["productVariant"]);
+            
+            ClassicAssert.AreEqual(titleproductVariant, productPage.GetProductVariantText(config["productVariant"]), "The titles do not match");
+        }
+    }
+    
+}
